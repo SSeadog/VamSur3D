51efefe8564cfafd63428ac14407b0ca02330907
DESKTOP-8VJVGPD\admin <ym20031223@gmail.com>
2023-01-27 16:15:03 +0900

    캐릭터 데미지 이펙트 사망모션

--- Assets/Scripts/Character/SwordHeroMove.cs
+++ Assets/Scripts/Character/SwordHeroMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 enum ESwordHeroMove
@@ -10,23 +11,59 @@ enum ESwordHeroMove
     a,//left
     s,//back
     d,//right
+    die,
 
 }
 public class SwordHeroMove : MonoBehaviour
 {
+    Color heroColor;
+    [SerializeField] SkinnedMeshRenderer _render;
+    [SerializeField] Animator _ani;
     [SerializeField] float speed;
-    Animator _ani;
-    
-    // Start is called before the first frame update
+    [SerializeField] GameObject _rotate;
+    [SerializeField] int _hp;
+    [SerializeField] GameObject _hero;
+    float _timer  = 0f;
+    bool _hit = false;
+    bool _isGameOver = false;
+    public void Hitted()
+    {
+        if (Input.GetKeyDown(KeyCode.M))//공격받았을때
+        {
+            _hp -= 20;
+            _hit = true;
+        }
+        if (_hp <= 0)
+        {
+            Die();
+        }
+    }
+   public void HittedColer()
+    {
+        if (_hit == true)
+        {
+            _timer += Time.deltaTime;
+            _render.material.color = Color.red;
+            if (_timer > 0.5f)
+            {
+                _render.material.color = heroColor;
+                _timer = 0f;
+                _hit = false;
+            }
+        }
+    }
     void Start()
     {
-        _ani = GetComponent<Animator>();
+        heroColor = _render.material.color;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (_isGameOver) return;
         move();
+        Hitted();
+        HittedColer();
+
     }
     public void move()
     {
@@ -35,24 +72,28 @@ public class SwordHeroMove : MonoBehaviour
         {
             v3 += (Vector3.forward).normalized * Time.deltaTime * speed;
             _ani.SetInteger("SwordHero", (int)ESwordHeroMove.w);
+            _rotate.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
             Debug.Log("forward");
         }
-       if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
-           v3 += (Vector3.left).normalized * Time.deltaTime * speed;
+            v3 += (Vector3.left).normalized * Time.deltaTime * speed;
             _ani.SetInteger("SwordHero", (int)ESwordHeroMove.w);
-             Debug.Log("left");
+            _rotate.transform.localRotation = Quaternion.Euler(new Vector3(0, 270, 0));
+            Debug.Log("left");
         }
         if (Input.GetKey(KeyCode.S))
         {
             v3 += (Vector3.back).normalized * Time.deltaTime * speed;
             _ani.SetInteger("SwordHero", (int)ESwordHeroMove.w);
+            _rotate.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
             Debug.Log("back");
         }
         if (Input.GetKey(KeyCode.D))
         {
             v3 += (Vector3.right).normalized * Time.deltaTime * speed;
             _ani.SetInteger("SwordHero", (int)ESwordHeroMove.w);
+            _rotate.transform.localRotation = Quaternion.Euler(new Vector3(0, 90, 0));
             Debug.Log("right");
         }
         if (v3 != Vector3.zero)
@@ -64,4 +105,18 @@ public class SwordHeroMove : MonoBehaviour
             _ani.SetInteger("SwordHero", (int)ESwordHeroMove.Idle);
         }
     }
+   
+    
+    
+    
+    
+    
+    public void Die()
+    {
+        _ani.SetInteger("SwordHero", (int)ESwordHeroMove.die);
+        _isGameOver = true;
+        Debug.Log("Die");
+    }
 }
+
+
