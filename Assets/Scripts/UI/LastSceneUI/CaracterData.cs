16f400ca876a917188cac4a65eb728e921ec8d07
DESKTOP-8VJVGPD\admin <ym20031223@gmail.com>
2023-04-28 15:50:12 +0900

    Hero 타입 메뉴씬에서 받아서 타입설정하고 데이터 저장하게 수정, 결과씬에서 정보 출력 되도록은함 (수정 할필요있음) Hero 스크립트 주석 Debug정리함  ,Hero State에서 Hitted 스테이트 삭제/ Hero에 Hitted 함수있음

--- Assets/Scripts/UI/LastSceneUI/CaracterData.cs
+++ Assets/Scripts/UI/LastSceneUI/CaracterData.cs
@@ -1,37 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class CaracterData : MonoBehaviour
 {
-
+    [SerializeField] Image heroImage;
     [SerializeField] Text _caracLV;
     [SerializeField] Text _livetimetx;
     [SerializeField] Text _goldtx;
     [SerializeField] Text _killstx;
     [SerializeField] Text _damagestx;
     [SerializeField] Text _null;
-    int _LV = 0;
-    float _livetime = 0f;
-    int _gold = 0;
-    int _kills = 0;
-    int _damages = 0;
+    Hero _hero;
     private void Start()
-
     {
-       _livetime = PlayerPrefs.GetFloat("_lifetime");//PlayerPrefs.setint("ÀÌ¸§"°ª),PlayerPrefs.getint("¹ÞÀ»ÀÌ¸§")
-       _damages = PlayerPrefs.GetInt("_totalDMG");
-        _LV = PlayerPrefs.GetInt("_LV");
-        _kills = PlayerPrefs.GetInt("_killcount");
-        Debug.Log(_livetime);
         Text();
+        Managers.Game.Clear();
     }
     public void Text()
     {
-        _caracLV.text = "·¹º§" + _LV;
+        _caracLV.text = "·¹º§" + Managers.Game.heroLv;
         _livetimetx.text =  $"{Managers.Game.surviveTime.ToString("F1")}"+"ÃÊ »ýÁ¸" ;//$¹®Àº Çüº¯È¯¿¡»ç¿ë
-        _goldtx.text = "È¹µæ°ñµå" + _gold;
-        _killstx.text = "ÃÑÅ³" + _kills;
-        _damagestx.text = "ÃÑµ¥¹ÌÁö" + _damages;
+        _goldtx.text = "È¹µæ°ñµå" + Managers.Game.stageGold;
+        _killstx.text = "ÃÑÅ³" + Managers.Game.killCount;
+        _damagestx.text = "ÃÑµ¥¹ÌÁö" + Managers.Game.totalDmg;
+        _hero = new Hero();
+        if (Managers.Game.heroType == Define.HeroType.Wizard)
+        {
+            heroImage.sprite = Resources.Load("Art/Textures/CharacterThumbnails/WizardThumb", typeof(Sprite)) as Sprite;
+        }
+        if (Managers.Game.heroType == Define.HeroType.SwordHero)
+        {
+            heroImage.sprite = Resources.Load("Art/Textures/CharacterThumbnails/SwordHeroThumb", typeof(Sprite)) as Sprite;
+        }
     }
 }
