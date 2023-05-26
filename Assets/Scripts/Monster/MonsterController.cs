7d8cb9ba9c5cf93ca6e1facfac3add8ced4f5a5e
rekaya <2unryoung@gmail.com>
2023-05-26 12:54:36 +0900

    Revert "엘리트 몬스터 소환조건 추가"
    This reverts commit 596860ff589b06296fe89f32494538a5ba9cde6d.

--- Assets/Scripts/Monster/MonsterController.cs
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class MonsterController : MonoBehaviour
 {
     bool isRespawnCoolTime = false;
-    bool isRespawn = false;
     //몬스터 강화 킬카운트
     //엘리트 몬스터 소환되는 킬카운트 200단위
     //몬스터 총량 킬카운트, 엘리트몬스터는 총량 하나로 고 정,
@@ -12,33 +11,23 @@ public class MonsterController : MonoBehaviour
     //임시 5씩 늘어남 일반몹, 투사체
     private void Start()
     {
-    
+        
     }
 
     private void Update()
     {
-        if(GenericSingleton<GameManager>.getInstance().KillCount % 5 != 0)
-        {
-            isRespawn = false;
-        }
         if (isRespawnCoolTime == false)
         {
             StartCoroutine(RespawnCollTime());
         }
-        if(GenericSingleton<GameManager>.getInstance().KillCount > 1 && GenericSingleton<GameManager>.getInstance().KillCount % 5 == 0 && isRespawn == false)
-        {
-            GenericSingleton<MonsterFactory>.getInstance().SummonEliteMonster();
-            isRespawn = true;
-        }
     }
-
 
     IEnumerator RespawnCollTime()
     {
         isRespawnCoolTime = true;
         GenericSingleton<MonsterFactory>.getInstance().SummonMonster();
         Debug.Log("소환");
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(10.0f);
         isRespawnCoolTime = false;
     }
 }
